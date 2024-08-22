5cafcfcebd5243f70da0cf055cfb7b808f90be59
Ramon Fossa Chiesi <ramon.fossa@gmail.com>
2024-08-21 22:50:12 -0300

    feat: Creates `Page` Model

---
+++ Destini-iOS13/Model/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  Destini-iOS13
+//
+//  Created by Ramon Fossa Chiesi on 21/08/24.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let label: String
+    let button1: String
+    let button2: String
+}
